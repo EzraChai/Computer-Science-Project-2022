dbe26dfe297df1269b695d7784f865b3284bce47
Ezra Chai <juanzhe2@gmail.com>
2022-09-17 17:23:12 +0800

    added env

--- resources/views/peserta/import.blade.php
+++ resources/views/peserta/import.blade.php
@@ -14,14 +14,14 @@
         <form method="POST" action="/dashboard/competition/{{$competition_id}}/participant/import" enctype="multipart/form-data">
             @csrf
                 <div class="mt-4">
-                    <div class=" w-full h-100 border border-dashed border-gray-500 relative">
+                    <div class=" w-full h-100 rounded-btn border border-dashed border-gray-500 relative">
                         <input type="file" name="uploaded_file" accept=".csv" onchange="handleSelectedFile(event)" multiple class="cursor-pointer relative block opacity-0 w-full h-full p-20 z-50">
                             <div class="text-center p-10 absolute top-0 right-0 left-0 m-auto">
                             <h4>
                                 Muatnaik fail di sini
                                 <br/>atau
                             </h4>
-                            <button class="btn btn-secondary">Pilih fail</button>
+                            <button class="btn btn-secondary mt-1">Pilih fail</button>
                             <div id="fileName" class="mt-2"/>
                         </div>
                     </div>
